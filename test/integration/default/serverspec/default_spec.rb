c097f5c4b196b513910e07340c58e1c50ec82746
Aditya Prakash <adityaprakash.bobby@gmail.com>
2019-12-15 23:50:16 +0530

    add few more tests for nginx

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 
 describe 'nginx_installation' do
     context package('nginx') do
+        
         it { should be_installed }
+        
+        context port(80) do
+            it { should be_listening }
+        end
+
+        context command('curl http://localhost/') do
+            its(:stdout) do
+                should contain 'Welcome to nginx'
+            end
+        end
+
     end
 end
